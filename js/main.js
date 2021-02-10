8acd2649346017312c0b874ea85391766e319023
Alexandra Pshebitkovskaya <alexandrapshebitkovskaya@gmail.com>
2021-02-10 16:59:35 +0200

    Fix base function, function coordinate to number

--- js/main.js
+++ js/main.js
@@ -1,53 +1,22 @@
-//Первое задание. Возвращает случайное целое число
-
-// Распишу как я поняла эту функцию, потому что долго не могла понять вот эти действия (max - min + 1) ) + min
-// Math.random возвращает число с плавающей запятой
-// Поэтому округляемм получаемое значение
-// max - min + 1  - задаем диапазон
-//  + min - указываем с какого числа диапазон начинается
-
-// https://schoolsw3.com/js/js_random.php
-const getRandomNumber1 = function (min, max) {
-  if (min < 0) {
-    return ('Error. First value cannot be less than zero')
+//Случайное число из диапазона включительно
+const getRandomNumber = function (min, max) {
+  if (min > 0 && min < max) {
+    const result = Math.random() * (max - min + 1) + min;
+    return result > max ? max : result;
   }
-  if (max <= min) {
-    return ('Error. Second value cannot be less than first');
-  }
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+  throw new Error('Firt value cannot be less than zero and bigger than first');
 }
 
-getRandomNumber1(3, 10);
-
-const getRandomNumber2 = function (min, max) {
-  return (min < 0 || max <= min) ? 'Error. First value cannot be less than zero and bigger than first' : Math.floor(Math.random() * (max - min + 1) ) + min;
+// Случайное целое число из диапазона включительно
+// eslint-disable-next-line no-unused-vars
+const getRandomInteger = function (min, max) {
+  const randomNumber = getRandomNumber(min, max);
+  return randomNumber ? Math.floor(randomNumber) : null;
 }
 
-getRandomNumber2(6, 13);
-
-const getRandomNumber3 = function (min, max) {
-  return (min < 0) ? 'Error. First value cannot be less than zero' :
-    (max <= min) ? 'Error. Second value cannot be less than first' :
-      Math.floor(Math.random() * (max - min + 1) ) + min;
-}
-
-getRandomNumber3(-5, 3);
-
-//Пример с https://dev-gang.ru/article/generacija-sluczainyh-czisel-s-vanilnym-js-9xozvi8tsm/
-// не понимаю почему последняя скобка закрывается в разных местах в первом примере и тут. Или во втором ошибка?
-
-const randomNumber = function (min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-randomNumber(2, 9)
-
-// Второе задание. Случайное число с плавающей запятой
-
+// Случайное число с плавающей запятой из диапазона включительно
+// eslint-disable-next-line no-unused-vars
 const getCoordinate = function(min, max, digits) {
-  return (min < 0) ? 'Error. First value cannot be less than zero' :
-    (max <= min) ? 'Error. Second value cannot be less than first' :
-      (Math.random() * (max - min + 1) + min).toFixed(digits);
+  const coordinate = getRandomNumber(min, max);
+  return coordinate ? +coordinate.toFixed(digits) : null;
 }
-
-getCoordinate(17, 19, 4);
